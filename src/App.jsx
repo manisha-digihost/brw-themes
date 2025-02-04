8721857069d73509e318b9654da54a76d430781b
manisha-digihost <manisha.prajapati@digihost.in>
2025-02-04 18:27:26 +0530

    remove errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import BaseTemplate from "./pages/BaseTemplate";
-// import Template1 from "./pages/Template1";
+import Template1 from "./pages/Template1";
 // import Template2 from "./pages/Template2";
 import { Routes, Route } from "react-router-dom";
 // import Template3 from "./pages/Template3";
--- src/pages/Template1.jsx
+++ src/pages/Template1.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ReactLenis, useLenis } from "lenis/react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Header1 from "../layout/Header/Header1";
 import Footer1 from "../layout/Footer/Footer1";
 import About1 from "../components/About/About1";
@@ -11,22 +10,21 @@ import Products1 from "../components/Products/Products1";
 import Testimonials1 from "../components/Testimonials/Testimonials1";
 import Gallery1 from "../components/Gallery/Gallery1";
 
-
 const Template1 = () => {
   return (
-  <ReactLenis root>
-    <div>
-      <Header1 />
-      <Hero1 />
-      <USPS1 />
-      <About1 />
-      <Products1 />
-      <Testimonials1 />
-      <Gallery1 />
-      <Contact1 />
-      <Footer1 />
-    </div>
-  </ReactLenis>
+    <ReactLenis root>
+      <div>
+        <Header1 />
+        <Hero1 />
+        <USPS1 />
+        <About1 />
+        <Products1 />
+        <Testimonials1 />
+        <Gallery1 />
+        <Contact1 />
+        <Footer1 />
+      </div>
+    </ReactLenis>
   );
 };
 
