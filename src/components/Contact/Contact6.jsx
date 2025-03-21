54ad21dd40d371635ea8dec364cb2bca103d35ac
manisha-digihost <manisha.prajapati@digihost.in>
2025-03-21 18:53:15 +0530

    template 6

--- src/components/Contact/Contact6.jsx
+++ src/components/Contact/Contact6.jsx
@@ -5,83 +5,113 @@ import { FaPhone } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 
-
-
 const Contact6 = () => {
   return (
     <section className="py-16 bg-light">
       <div className="container mx-auto px-4">
         <div className="flex flex-col gap-5 md:flex md:flex-row">
           <div className="w-full">
-            <h1 className="text-6xl font-black uppercase pb-[50px]">Let’s connect support team</h1>
-            <span className="text-red-500 border-b-4 border-red-500 text-2xl font-semibold">Contact with us</span>
+            {/* <h1 className="text-6xl font-black uppercase pb-[50px]">
+              Let’s connect support team
+            </h1>
+            <span className="text-red-500 border-b-4 border-red-500 text-2xl font-semibold">
+              Contact with us
+            </span> */}
+            <div className="w-[65%] mx-auto">
+              <div className="text-center bs5-heading-sec">
+                <h2 className="bs5-heading">
+                  Our <span>Gallery</span>
+                </h2>
+                {/* <p>
+              While mirth large of on front. Ye he greater related adapted
+              proceed entered an. Through it examine express promise no. Past
+              add size game cold girl off how old
+            </p> */}
+              </div>
+            </div>
             <div className="pt-[50px] flex flex-wrap gap-5 items-center">
-              <span className="text-xl font-semibold"><FaLocationDot /></span>
-              <a href="#!" className="text-xl font-semibold"> 1 800 222 4357</a>
+              <span className="text-xl font-semibold">
+                <FaLocationDot />
+              </span>
+              <a href="#!" className="text-xl font-semibold">
+                {" "}
+                1 800 222 4357
+              </a>
             </div>
             <div className="flex flex-wrap gap-5 items-center mt-5">
-              <span className="text-xl font-semibold"><FaPhone /></span>
-              <p className="text-xl font-semibold"> 25 Palmal Stree, London, England</p>
+              <span className="text-xl font-semibold">
+                <FaPhone />
+              </span>
+              <p className="text-xl font-semibold">
+                {" "}
+                25 Palmal Stree, London, England
+              </p>
             </div>
 
             <div className="flex flex-wrap gap-5 items-center mt-5">
-              <span className="text-xl font-semibold"><IoMdMail /></span>
-              <a href="#!" className="text-xl font-semibold"> webtend24@gmail.com</a>
+              <span className="text-xl font-semibold">
+                <IoMdMail />
+              </span>
+              <a href="#!" className="text-xl font-semibold">
+                {" "}
+                webtend24@gmail.com
+              </a>
             </div>
           </div>
           <div className="w-full">
             <h3 className="text-2xl font-semibold mb-6">Get in touch </h3>
 
-            <form action="">           
+            <form action="">
               <div className="col-span-full">
-              <div className="mt-2">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                  className="h-[200px] block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-300 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  sm:text-sm/6"
-                  placeholder="Write your message here..."
-                  defaultValue={''}
-                />
+                <div className="mt-2">
+                  <textarea
+                    id="about"
+                    name="about"
+                    rows={3}
+                    className="h-[200px] block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-300 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  sm:text-sm/6"
+                    placeholder="Write your message here..."
+                    defaultValue={""}
+                  />
+                </div>
               </div>
-            </div>
 
-            <div className="flex gap-8 mt-5">
+              <div className="flex gap-8 mt-5">
+                <div className="sm:col-span-3 w-full lg:w-1/2">
+                  <div className="mt-2">
+                    <input
+                      id="last-name"
+                      name="last-name"
+                      type="text"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md bg-white px-3 py-5 text-base border border-gray-300 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  sm:text-sm/6"
+                      placeholder="Your Name"
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
 
-            <div className="sm:col-span-3 w-full lg:w-1/2">
-              <div className="mt-2">
-                <input
-                  id="last-name"
-                  name="last-name"
-                  type="text"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md bg-white px-3 py-5 text-base border border-gray-300 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  sm:text-sm/6"
-                  placeholder="Your Name"
-                  defaultValue={''}
-                />
+                <div className="sm:col-span-4 w-full lg:w-1/2">
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      className="block w-full rounded-md bg-white px-3 py-5 text-base border border-gray-300  text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  sm:text-sm/6"
+                      placeholder="Your Email"
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-
-            <div className="sm:col-span-4 w-full lg:w-1/2">
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md bg-white px-3 py-5 text-base border border-gray-300  text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  sm:text-sm/6"
-                  placeholder="Your Email"
-                  defaultValue={''}
-                />
-              </div>
-            </div>
-
-            </div>
-         
             </form>
-         
-          <a href="#!" className="inline-block mt-5 px-10 py-5 text-white font-bold transition duration-300 hover:bg-green-600 bg-red-600">Send Message</a>
 
+            <a
+              href="#!"
+              className="inline-block mt-5 px-10 py-5 text-white font-bold transition duration-300 hover:bg-green-600 bg-red-600"
+            >
+              Send Message
+            </a>
           </div>
         </div>
       </div>
--- src/components/Gallery/Gallery6.jsx
+++ src/components/Gallery/Gallery6.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import img1 from "./../../assets/images/gallery/gallery1.webp"
-import img2 from "./../../assets/images/gallery/gallery2.webp"
-import img3 from "./../../assets/images/gallery/gallery3.webp"
-import img4 from "./../../assets/images/gallery/gallery4.webp"
-import img5 from "./../../assets/images/gallery/gallery5.webp"
-import img7 from "./../../assets/images/gallery/gallery7.webp"
+import img1 from "./../../assets/images/gallery/gallery1.webp";
+import img2 from "./../../assets/images/gallery/gallery2.webp";
+import img3 from "./../../assets/images/gallery/gallery3.webp";
+import img4 from "./../../assets/images/gallery/gallery4.webp";
+import img5 from "./../../assets/images/gallery/gallery5.webp";
+import img7 from "./../../assets/images/gallery/gallery7.webp";
 
 const Gallery6 = () => {
   const [category, setCategory] = useState("all");
@@ -15,30 +15,46 @@ const Gallery6 = () => {
     { id: 3, category: "nature", thumbnail: img3 },
     { id: 4, category: "animals", thumbnail: img4 },
     { id: 5, category: "architecture", thumbnail: img5 },
-    { id: 6, category: "animals", thumbnail: img7  },
+    { id: 6, category: "animals", thumbnail: img7 },
   ];
 
-  const filteredImages = category === "all" 
-    ? galleryImages 
-    : galleryImages.filter((image) => image.category === category);
+  const filteredImages =
+    category === "all"
+      ? galleryImages
+      : galleryImages.filter((image) => image.category === category);
 
   return (
     <section className="py-12">
       <div className="container mx-auto px-4">
         {/* Heading */}
-        <div className="mb-9 w-lg">
+        {/* <div className="mb-9 w-lg">
               <h2 className="text-5xl  uppercase font-bold mb-2">Happy Client Says About our company</h2>
               <span className="text-sm text-red-600 underline ">Some Standard Serivices</span>
-            </div>
-
+            </div> */}
+        <div className="w-[65%] mx-auto mb-10">
+          <div className="text-center bs5-heading-sec">
+            <h2 className="bs5-heading">
+              Our <span>Gallery</span>
+            </h2>
+            <p>
+              While mirth large of on front. Ye he greater related adapted
+              proceed entered an. Through it examine express promise no. Past
+              add size game cold girl off how old
+            </p>
+          </div>
+        </div>
         {/* Filter Buttons */}
         <div className="flex justify-center space-x-3 mb-6">
           {["all", "nature", "architecture", "animals"].map((filter) => (
             <button
               key={filter}
               onClick={() => setCategory(filter)}
-              className={`px-4 py-2 rounded-full transition 
-                ${category === filter ? "bg-red text-black bg-gray-200" : "border hover:bg-denger hover:text-black"}`}
+              className={`px-4 py-2 rounded-full transition active:bg-red-500                                                   
+                ${
+                  category === filter
+                    ? "bg-red text-black bg-gray-200"
+                    : "border hover:bg-denger hover:text-black"
+                }`}
             >
               {filter.charAt(0).toUpperCase() + filter.slice(1)}
             </button>
@@ -49,10 +65,10 @@ const Gallery6 = () => {
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {filteredImages.map((image) => (
             <div key={image.id} className="relative overflow-hidden rounded-lg">
-              <img 
-                src={image.thumbnail} 
+              <img
+                src={image.thumbnail}
                 alt="Gallery"
-                className="w-full h-80 object-cover transition-transform transform hover:scale-105" 
+                className="w-full h-80 object-cover transition-transform transform hover:scale-105"
               />
             </div>
           ))}
