112c2157969e78fbb785491b4b1eddaececcbae2
manisha-digihost <manisha.prajapati@digihost.in>
2025-03-21 13:34:26 +0530

    created testimonials

--- src/components/Testimonials/Testimonials6.jsx
+++ src/components/Testimonials/Testimonials6.jsx
@@ -1,121 +1,189 @@
 import React, { useRef, useState } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import videobanner from "./../../assets/images/video-bg.webp"
+import { Swiper, SwiperSlide } from "swiper/react";
+import videobanner from "./../../assets/images/video-bg.webp";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-import 'swiper/css/thumbs';
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { Pagination } from "swiper/modules";
+// Import Swiper React components
+// import { Swiper, SwiperSlide } from 'swiper/react';
+import { FaQuoteLeft } from "react-icons/fa";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 // import './styles.css';
 
 // import required modules
-import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 
 const Testimonials6 = () => {
-  const testimonials = [
-    {
-      text: "She were green under to multiply over teen stars seen livine second and whose blessed begining give from evening the saying light darkness give saying. about in sea given fly third abundantly must forgivide",
-      name: "Daniel Richard",
-      role: "CEO, Apple",
-      image: "https://randomuser.me/api/portraits/men/10.jpg",
-    },
-    {
-      text: "She were green under to multiply over teen stars seen livine second and whose blessed begining give from evening the saying light darkness give saying. about in sea given fly third abundantly must forgivide",
-      name: "Daniel Richard",
-      role: "CEO, Apple",
-      image: "https://randomuser.me/api/portraits/men/10.jpg",
-    },
-    {
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, suscipit. Laboriosam distinctio sunt ipsa? Rem perferendis assumenda laborum recusandae.",
-      name: "Emma Johnson",
-      role: "CTO, Microsoft",
-      image: "https://randomuser.me/api/portraits/men/20.jpg",
-    },
-    {
-      text: "Exercitationem atque accusantium repellendus alias laudantium nisi architecto obcaecati tempora quibusdam in cumque iste dolore, optio beatae.",
-      name: "James Brown",
-      role: "Lead Designer, Google",
-      image: "https://randomuser.me/api/portraits/men/20.jpg",
-    },
-  ];
-
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
-
+  // const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   return (
     <section className="bg-gray-100 py-20">
       <div className="container mx-auto px-4">
-        <div className="lg:grid md:grid-cols-2 gap-8 sm:block">
-          <div className="w-100">
-            <div className="mb-9">
-              <h2 className="text-5xl   uppercase font-bold mb-2">Happy Client Says About our company</h2>
-              <span className="text-sm text-red-600 underline ">Some Standard Serivices</span>
-            </div>
-            <div className="flex justify-center">
-              <div className="w-full max-w-3xl">
-                <Swiper
-                  onSwiper={setThumbsSwiper}
-                  spaceBetween={10}
-                  slidesPerView={3}
-                  freeMode={true}
-                  watchSlidesProgress={true}
-                  modules={[FreeMode, Navigation, Thumbs]}
-                  className="mySwiper mb-8"
-                  breakpoints={{
-                    320: { slidesPerView: 1, spaceBetween: 10 }, // Mobile: 1 slide
-                    768: { slidesPerView: 2, spaceBetween: 20 }, // Tablet: 2 slides
-                    1024: { slidesPerView: 3, spaceBetween: 30 }, // Desktop: 3 slides
-                  }}
-                >
-                  {
-                    testimonials.map((item1) => (
-                      <SwiperSlide>
-                        <img className="rounded-full" src={item1.image} />
-                      </SwiperSlide>
-                    ))
-                  }
-                </Swiper>
-                <Swiper
-                  style={{
-                    '--swiper-navigation-color': '#fff',
-                    '--swiper-pagination-color': '#fff',
-                  }}
-                  spaceBetween={10}
-                  navigation={false}
-                  thumbs={{ swiper: thumbsSwiper }}
-                  modules={[FreeMode, Navigation, Thumbs]}
-                  className="mySwiper2"
-                >
-                  {
-                    testimonials.map((item) => (
-                      <SwiperSlide>
-                        <div className="">
-                          <p className="mb-6">
-                            {item.text}
-                          </p>
-                          <div className="">
-                            <h5 className="text-lg font-bold">{item.name}</h5>
-                            <span className="text-sm">{item.role}</span>
-                          </div>
-                        </div>
-                      </SwiperSlide>
-                    ))
-                  }
-                </Swiper>
-              </div>
-            </div>
-          </div>
-          <div className=" ">
-            <figure className="lg:mt-0 mt-8 block">
-              <img className="lg:rounded-tl-[23vw]" src={videobanner} alt="" />
-            </figure>
+        <div className="w-[65%] mx-auto">
+          <div className="text-center bs5-heading-sec">
+            <h2 className="bs5-heading">
+              Patient <span>Testimonials</span>
+            </h2>
+            <p>
+              While mirth large of on front. Ye he greater related adapted
+              proceed entered an. Through it examine express promise no. Past
+              add size game cold girl off how old
+            </p>
           </div>
         </div>
-
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          // pagination={{
+          //   clickable: true,
+          // }}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 2,
+              spaceBetween: 50,
+            },
+          }}
+          // modules={[Pagination]}
+          className="mySwiper mt-16"
+        >
+          <SwiperSlide className="relative p-10">
+            <div className="absolute top-[18px] left-[110px] text-5xl text-[#0cb8b6]">
+              <FaQuoteLeft />
+            </div>
+            <div className="card p-20 border rounded-3xl">
+              <p className="text-[#666666] text-[18px]">
+                Departure so attention pronounce satisfied daughters am. But shy
+                tedious pressed studied opinion entered windows off. Advantage
+                dependent suspicion convinced provision him yet. Mr immediate
+                remaining conveying allowance do or.
+              </p>
+              <hr className="my-10" />
+              <div className="flex gap-5">
+                <div className="rounded-full overflow-hidden w-[60px] h-[60px] ">
+                  <img
+                    src="https://validthemes.net/site-template/medihub/assets/img/team/7.jpg"
+                    alt="Person"
+                  />
+                </div>
+                <div className="">
+                  <h4 className="text-[#04000b] text-[22px] mb-1">
+                    John Abraham
+                  </h4>
+                  <h5 className="text-[#666666] uppercase">
+                    patient of <span className="text-[#0cb8b6]">surgery</span>
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="relative p-10">
+            <div className="absolute top-[18px] left-[110px] text-5xl text-[#0cb8b6]">
+              <FaQuoteLeft />
+            </div>
+            <div className="card p-20 border rounded-3xl">
+              <p className="text-[#666666] text-[18px]">
+                Departure so attention pronounce satisfied daughters am. But shy
+                tedious pressed studied opinion entered windows off. Advantage
+                dependent suspicion convinced provision him yet. Mr immediate
+                remaining conveying allowance do or.
+              </p>
+              <hr className="my-10" />
+              <div className="flex gap-5">
+                <div className="rounded-full overflow-hidden w-[60px] h-[60px] ">
+                  <img
+                    src="https://validthemes.net/site-template/medihub/assets/img/team/7.jpg"
+                    alt="Person"
+                  />
+                </div>
+                <div className="">
+                  <h4 className="text-[#04000b] text-[22px] mb-1">
+                    John Abraham
+                  </h4>
+                  <h5 className="text-[#666666] uppercase">
+                    patient of <span className="text-[#0cb8b6]">surgery</span>
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="relative p-10">
+            <div className="absolute top-[18px] left-[110px] text-5xl text-[#0cb8b6]">
+              <FaQuoteLeft />
+            </div>
+            <div className="card p-20 border rounded-3xl">
+              <p className="text-[#666666] text-[18px]">
+                Departure so attention pronounce satisfied daughters am. But shy
+                tedious pressed studied opinion entered windows off. Advantage
+                dependent suspicion convinced provision him yet. Mr immediate
+                remaining conveying allowance do or.
+              </p>
+              <hr className="my-10" />
+              <div className="flex gap-5">
+                <div className="rounded-full overflow-hidden w-[60px] h-[60px] ">
+                  <img
+                    src="https://validthemes.net/site-template/medihub/assets/img/team/7.jpg"
+                    alt="Person"
+                  />
+                </div>
+                <div className="">
+                  <h4 className="text-[#04000b] text-[22px] mb-1">
+                    John Abraham
+                  </h4>
+                  <h5 className="text-[#666666] uppercase">
+                    patient of <span className="text-[#0cb8b6]">surgery</span>
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="relative p-10">
+            <div className="absolute top-[18px] left-[110px] text-5xl text-[#0cb8b6]">
+              <FaQuoteLeft />
+            </div>
+            <div className="card p-20 border rounded-3xl">
+              <p className="text-[#666666] text-[18px]">
+                Departure so attention pronounce satisfied daughters am. But shy
+                tedious pressed studied opinion entered windows off. Advantage
+                dependent suspicion convinced provision him yet. Mr immediate
+                remaining conveying allowance do or.
+              </p>
+              <hr className="my-10" />
+              <div className="flex gap-5">
+                <div className="rounded-full overflow-hidden w-[60px] h-[60px] ">
+                  <img
+                    src="https://validthemes.net/site-template/medihub/assets/img/team/7.jpg"
+                    alt="Person"
+                  />
+                </div>
+                <div className="">
+                  <h4 className="text-[#04000b] text-[22px] mb-1">
+                    John Abraham
+                  </h4>
+                  <h5 className="text-[#666666] uppercase">
+                    patient of <span className="text-[#0cb8b6]">surgery</span>
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
-
     </section>
   );
 };
