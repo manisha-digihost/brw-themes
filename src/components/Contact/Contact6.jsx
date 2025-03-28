b423773c4362b20399a6db9b255e2af32c9642ac
manisha-digihost <manisha.prajapati@digihost.in>
2025-03-28 16:18:45 +0530

    created gallery and contact

--- src/components/Contact/Contact6.jsx
+++ src/components/Contact/Contact6.jsx
@@ -4,77 +4,38 @@ import { LuClock4 } from "react-icons/lu";
 import { FaPhone } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
+import { IoIosPaperPlane } from "react-icons/io";
 
 const Contact6 = () => {
   return (
-    <section className="py-16 bg-light">
+    <section className="py-16 bg-[#14457b]">
       <div className="container mx-auto px-4">
         <div className="flex flex-col gap-5 md:flex md:flex-row">
-          <div className="w-full">
-            {/* <h1 className="text-6xl font-black uppercase pb-[50px]">
-              Let’s connect support team
-            </h1>
-            <span className="text-red-500 border-b-4 border-red-500 text-2xl font-semibold">
-              Contact with us
-            </span> */}
-            <div className="w-[65%] mx-auto">
-              <div className="text-center bs5-heading-sec">
-                <h2 className="bs5-heading">
-                  Our <span>Gallery</span>
-                </h2>
-                {/* <p>
-              While mirth large of on front. Ye he greater related adapted
-              proceed entered an. Through it examine express promise no. Past
-              add size game cold girl off how old
-            </p> */}
-              </div>
-            </div>
-            <div className="pt-[50px] flex flex-wrap gap-5 items-center">
-              <span className="text-xl font-semibold">
-                <FaLocationDot />
-              </span>
-              <a href="#!" className="text-xl font-semibold">
-                {" "}
-                1 800 222 4357
-              </a>
-            </div>
-            <div className="flex flex-wrap gap-5 items-center mt-5">
-              <span className="text-xl font-semibold">
-                <FaPhone />
-              </span>
-              <p className="text-xl font-semibold">
-                {" "}
-                25 Palmal Stree, London, England
+          <div className="w-[80%] mx-auto">
+            <div className="text-center mb-10">
+              <h3 className="text-[50px] font-semibold mb-6 text-light text-center">
+                Get in touch{" "}
+              </h3>
+              <p className="text-light">
+                Our next drew much you with rank. Tore many held age hold rose
+                than our. She literature sentiments any contrasted. Set aware
+                joy sense young now tears china shy.
               </p>
             </div>
-
-            <div className="flex flex-wrap gap-5 items-center mt-5">
-              <span className="text-xl font-semibold">
-                <IoMdMail />
-              </span>
-              <a href="#!" className="text-xl font-semibold">
-                {" "}
-                webtend24@gmail.com
-              </a>
-            </div>
-          </div>
-          <div className="w-full">
-            <h3 className="text-2xl font-semibold mb-6">Get in touch </h3>
-
-            <form action="">
+            <form action="" className="">
               <div className="col-span-full">
                 <div className="mt-2">
-                  <textarea
-                    id="about"
-                    name="about"
-                    rows={3}
-                    className="h-[200px] block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-300 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  sm:text-sm/6"
-                    placeholder="Write your message here..."
+                  <input
+                    id="last-name"
+                    name="last-name"
+                    type="text"
+                    autoComplete="family-name"
+                    className="block w-full rounded-[4px] bg-transparent px-5 py-4 text-base border border-[#e9edf273]  outline-0 text-light placeholder:text-gray-400  sm:text-sm/6"
+                    placeholder="Name"
                     defaultValue={""}
                   />
                 </div>
               </div>
-
               <div className="flex gap-8 mt-5">
                 <div className="sm:col-span-3 w-full lg:w-1/2">
                   <div className="mt-2">
@@ -83,13 +44,12 @@ const Contact6 = () => {
                       name="last-name"
                       type="text"
                       autoComplete="family-name"
-                      className="block w-full rounded-md bg-white px-3 py-5 text-base border border-gray-300 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  sm:text-sm/6"
+                      className="block w-full rounded-[4px] bg-transparent px-5 py-4 text-base border border-[#e9edf273] outline-0 text-light placeholder:text-gray-400  sm:text-sm/6"
                       placeholder="Your Name"
                       defaultValue={""}
                     />
                   </div>
                 </div>
-
                 <div className="sm:col-span-4 w-full lg:w-1/2">
                   <div className="mt-2">
                     <input
@@ -97,21 +57,40 @@ const Contact6 = () => {
                       name="email"
                       type="email"
                       autoComplete="email"
-                      className="block w-full rounded-md bg-white px-3 py-5 text-base border border-gray-300  text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400  sm:text-sm/6"
+                      className="block w-full rounded-[4px] bg-transparent px-5 py-4 text-base border border-[#e9edf273] outline-0 text-light placeholder:text-gray-400  sm:text-sm/6"
                       placeholder="Your Email"
                       defaultValue={""}
                     />
                   </div>
                 </div>
               </div>
+              <div className="col-span-full">
+                <div className="mt-4">
+                  <textarea
+                    id="about"
+                    name="about"
+                    rows={3}
+                    className="h-[200px] block w-full rounded-[4px] bg-transparent px-5 py-4 text-base  border border-[#e9edf273] outline-0 text-light  sm:text-sm/6"
+                    placeholder="Write your message here..."
+                    defaultValue={""}
+                  />
+                </div>
+              </div>
             </form>
-
-            <a
+            {/* <a
               href="#!"
               className="inline-block mt-5 px-10 py-5 text-white font-bold transition duration-300 hover:bg-green-600 bg-red-600"
             >
               Send Message
-            </a>
+            </a> */}
+            <div className="flex items-center justify-center mx-auto">
+              <button
+                type="submit"
+                className="text-white py-4 transition-all ease-in-out px-8 bg-[#0cb8b6] hover:bg-black hover:text-white w-fit rounded-full mt-6 flex justify-center items-center gap-2"
+              >
+                Send Message <IoIosPaperPlane />
+              </button>
+            </div>
           </div>
         </div>
       </div>
