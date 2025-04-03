5b19420090e1a55e120fc8cc7cf8934d9d186127
manisha-digihost <manisha.prajapati@digihost.in>
2025-04-03 12:04:26 +0530

    template 7

--- src/components/Hero/Hero7.jsx
+++ src/components/Hero/Hero7.jsx
@@ -10,25 +10,25 @@ import "swiper/css/pagination";
 const Hero7 = () => {
   const slides = [
     {
-      title: "Example headline.",
-      description:
-        "Some representative placeholder content for the first slide of the carousel.",
+      title: "Dedicated Team Exceptional unique architecture Design Idea",
+      // description:
+      //   "Some representative placeholder content for the first slide of the carousel.",
       buttonText: "Sign up today",
       buttonLink: "#",
       image: "src/assets/images/hero/1.jpg",
     },
     {
-      title: "Another example headline.",
-      description:
-        "Some representative placeholder content for the second slide of the carousel.",
+      title: "Dedicated Team Exceptional unique architecture Design Idea",
+      // description:
+      //   "Some representative placeholder content for the second slide of the carousel.",
       buttonText: "Learn more",
       buttonLink: "#",
       image: "src/assets/images/hero/2.jpg",
     },
     {
-      title: "One more for good measure.",
-      description:
-        "Some representative placeholder content for the third slide of this carousel.",
+      title: "Dedicated Team Exceptional unique architecture Design Idea",
+      // description:
+      //   "Some representative placeholder content for the third slide of this carousel.",
       buttonText: "Browse gallery",
       buttonLink: "#",
       image: "src/assets/images/hero/1.jpg",
@@ -51,13 +51,14 @@ const Hero7 = () => {
               <div className="absolute inset-0">
                 <img src={slide.image} alt="Hero" />
               </div>
-              <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white p-4">
-                <h1 className="text-4xl md:text-5xl font-bold mb-4 text-light">
+              <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white p-4 w-[70%] mx-auto">
+                <h1 className="text-[50px] md:text-[50px] font-bold mb-4 text-light">
                   {slide.title}
                 </h1>
-                <p className="text-lg md:text-xl opacity-75 mb-6 max-w-2xl text-lightAlt">
+                {/* <h2 class="text-4xl md:text-5xl font-bold mb-4 text-light"></h2> */}
+                {/* <p className="text-lg md:text-xl opacity-75 mb-6 max-w-2xl text-lightAlt">
                   {slide.description}
-                </p>
+                </p> */}
                 <a href={slide.buttonLink} className="btn-primary">
                   {slide.buttonText}
                 </a>
