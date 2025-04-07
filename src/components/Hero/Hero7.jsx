af22bd1e1f1c1f4cede63392c522198554e45a13
manisha-digihost <manisha.prajapati@digihost.in>
2025-04-07 12:02:02 +0530

    created hero

--- src/components/Hero/Hero7.jsx
+++ src/components/Hero/Hero7.jsx
@@ -13,7 +13,7 @@ const Hero7 = () => {
       title: "Dedicated Team Exceptional unique architecture Design Idea",
       // description:
       //   "Some representative placeholder content for the first slide of the carousel.",
-      buttonText: "Sign up today",
+      buttonText: "More Details",
       buttonLink: "#",
       image: "src/assets/images/hero/1.jpg",
     },
@@ -21,7 +21,7 @@ const Hero7 = () => {
       title: "Dedicated Team Exceptional unique architecture Design Idea",
       // description:
       //   "Some representative placeholder content for the second slide of the carousel.",
-      buttonText: "Learn more",
+      buttonText: "More Details",
       buttonLink: "#",
       image: "src/assets/images/hero/2.jpg",
     },
@@ -29,38 +29,47 @@ const Hero7 = () => {
       title: "Dedicated Team Exceptional unique architecture Design Idea",
       // description:
       //   "Some representative placeholder content for the third slide of this carousel.",
-      buttonText: "Browse gallery",
+      buttonText: "More Details",
       buttonLink: "#",
       image: "src/assets/images/hero/1.jpg",
     },
   ];
 
   return (
-    <div className="relative h-[500px] mb-6">
+    <div className="relative h-screen mb-6">
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
-        navigation
-        pagination={{ clickable: true }}
+        // navigation
+        // pagination={{ clickable: true }}
         autoplay={{ delay: 5000 }}
         loop={true}
         className="h-full"
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index}>
-            <div className="relative h-full bg-darkAlt">
+            <div className="relative h-full bg-darkAlt after:content-[''] after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-black after:opacity-[0.8]">
               <div className="absolute inset-0">
                 <img src={slide.image} alt="Hero" />
               </div>
-              <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white p-4 w-[70%] mx-auto">
-                <h1 className="text-[50px] md:text-[50px] font-bold mb-4 text-light">
+              <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white p-4 w-[65%] mx-auto z-10 uppercase">
+                <h1 className="text-[3.9rem] font-bold mb-4 text-light">
                   {slide.title}
                 </h1>
-                {/* <h2 class="text-4xl md:text-5xl font-bold mb-4 text-light"></h2> */}
-                {/* <p className="text-lg md:text-xl opacity-75 mb-6 max-w-2xl text-lightAlt">
-                  {slide.description}
-                </p> */}
-                <a href={slide.buttonLink} className="btn-primary">
+                <a href={slide.buttonLink} className="bs7-button">
                   {slide.buttonText}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="40"
+                    height="41"
+                    viewBox="0 0 40 41"
+                    fill="none"
+                    className="svg2"
+                  >
+                    <path
+                      d="M26.2138 14.8108C26.2136 14.8108 26.2133 14.8107 26.213 14.8107L21.1623 14.8229C20.784 14.8239 20.4779 15.1313 20.4788 15.5097C20.4797 15.8881 20.7872 16.1941 21.1656 16.1932L24.5565 16.185L14.2966 26.4449C14.0291 26.7125 14.0291 27.1463 14.2966 27.4138C14.5642 27.6814 14.998 27.6814 15.2656 27.4138L25.5254 17.154L25.5172 20.5448C25.5163 20.9232 25.8224 21.2307 26.2007 21.2316C26.5792 21.2325 26.8866 20.9264 26.8875 20.5481L26.8998 15.4975C26.8998 15.4972 26.8997 15.4969 26.8997 15.4966C26.9001 15.1175 26.5918 14.8103 26.2138 14.8108Z"
+                      fill="#fff"
+                    ></path>
+                  </svg>
                 </a>
               </div>
             </div>
--- src/layout/Header/Header7.jsx
+++ src/layout/Header/Header7.jsx
@@ -1,128 +1,140 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/theme6logo.png";
+import logo from "../../assets/images/bs7-logo.png";
 import { FaPlus } from "react-icons/fa6";
 import { GoSearch } from "react-icons/go";
+import { FiPhoneCall } from "react-icons/fi";
+import { FiMail } from "react-icons/fi";
+import { FaFacebookF, FaInstagram, FaPinterestP } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Header7 = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-white absolute top-0 left-0 right-0 py-3 z-20">
-      <div className="container mx-auto px-4">
-        <div className="flex justify-between items-center">
+    <nav className="bg-black absolute top-0 left-0 right-0 z-20 mx-10">
+      <div className="container">
+        <div className="flex justify-between items-center px-4 gap-0">
           <Link to="#home" className="text-2xl font-bold">
             <img src={logo} alt="Logo" />
           </Link>
-
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="lg:hidden focus:outline-none"
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-              />
-            </svg>
-          </button>
-
-          <div
-            className={`lg:flex items-center justify-between w-full ${
-              isOpen ? "block" : "hidden"
-            }`}
-          >
-            <div className="flex flex-col lg:flex-row lg:mx-auto gap-10">
-              <Link
-                to="#home"
-                className="px-3 py-2 text-black font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
-              >
-                Home
-              </Link>
-              <Link
-                to="#about"
-                className="px-3 py-2 text-black font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
-              >
-                About
-              </Link>
-
-              <div className="relative group">
-                <button className="flex items-center gap-1 px-3 py-2 text-black font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in">
-                  Services
-                  {/* <svg
-                    className="w-4 h-4 ml-1"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M19 9l-7 7-7-7"
-                    />
-                  </svg> */}
-                  <FaPlus />
-                </button>
-                <div className="absolute hidden group-hover:block bg-white shadow-lg py-2 rounded-lg">
+          <div className="flex flex-col">
+            <div className="flex justify-between gap-3 py-3 px-4">
+              <div className="flex gap-7">
+                <Link to="#" className="text-white flex gap-2 items-center">
+                  <FiPhoneCall className="text-[#a6a182]" /> +1 (550) 250 5260
+                </Link>
+                <Link to="#" className="text-white flex gap-2 items-center">
+                  <FiMail className="text-[#a6a182]" /> info@tecture.com
+                </Link>
+              </div>
+              <div className="flex items-center gap-9">
+                <Link to="#" className="text-white flex gap-2 items-center">
+                  <FiPhoneCall className="text-[#a6a182]" /> Mon - Fri: 09:00 -
+                  05:00
+                </Link>
+                <div className="flex gap-2">
                   <Link
-                    to="#products"
-                    className="block px-4 py-2 hover:bg-gray-100"
+                    to="#!"
+                    className="bg-[#363739] text-white rounded-full h-[40px] w-[40px] flex items-center justify-center"
                   >
-                    Products
+                    <FaFacebookF />
                   </Link>
                   <Link
-                    to="#testimonials"
-                    className="block px-4 py-2 hover:bg-gray-100"
+                    to="#!"
+                    className="bg-[#363739] text-white rounded-full h-[40px] w-[40px] flex items-center justify-center"
                   >
-                    Testimonials
+                    <FaInstagram />
                   </Link>
                   <Link
-                    to="#gallery"
-                    className="block px-4 py-2 hover:bg-gray-100"
+                    to="#!"
+                    className="bg-[#363739] text-white rounded-full h-[40px] w-[40px] flex items-center justify-center"
                   >
-                    Gallery
+                    <FaXTwitter />
+                  </Link>
+                  <Link
+                    to="#!"
+                    className="bg-[#363739] text-white rounded-full h-[40px] w-[40px] flex items-center justify-center"
+                  >
+                    <FaPinterestP />
                   </Link>
                 </div>
               </div>
-
-              <Link
-                to="#contact"
-                className="px-3 py-2 text-black font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
-              >
-                Contact
-              </Link>
-              <Link
-                to="#contact"
-                className="px-3 py-2 text-black font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
-              >
-                Gallery
-              </Link>
-              <Link
-                to="/"
-                className="px-3 py-2 text-black font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
-              >
-                Template1
-              </Link>
             </div>
+            <div
+              className={`lg:flex items-center justify-between w-full bg-white py-4 px-4 ${
+                isOpen ? "block" : "hidden"
+              }`}
+            >
+              <div className="flex flex-col lg:flex-row lg:mx-auto gap-10">
+                <Link
+                  to="#home"
+                  className="px-3 py-2 text-dark font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="#about"
+                  className="px-3 py-2 text-dark font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
+                >
+                  About
+                </Link>
 
-            <div className="end-links">
-              <div className="flex gap-8 items-center">
-                <Link to="#" className="text-2xl">
-                  <GoSearch />
+                <div className="relative group">
+                  <button className="flex items-center gap-1 px-3 py-2 text-dark font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in">
+                    Services
+                    <FaPlus />
+                  </button>
+                  <div className="absolute hidden group-hover:block bg-white shadow-lg py-2 rounded-lg">
+                    <Link
+                      to="#products"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                    >
+                      Products
+                    </Link>
+                    <Link
+                      to="#testimonials"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                    >
+                      Testimonials
+                    </Link>
+                    <Link
+                      to="#gallery"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                    >
+                      Gallery
+                    </Link>
+                  </div>
+                </div>
+
+                <Link
+                  to="#contact"
+                  className="px-3 py-2 text-dark font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
+                >
+                  Contact
                 </Link>
-                <Link to="#" className="flex flex-col gap-[6px] text-2xl">
-                  <span className="w-[2rem] block h-[3px] bg-black"></span>
-                  <span className="w-[1.2rem] block h-[2px] bg-black"></span>
-                  <span className="w-[1.7rem] block h-[3px] bg-black"></span>
+                <Link
+                  to="#contact"
+                  className="px-3 py-2 text-dark font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
+                >
+                  Gallery
                 </Link>
+                <Link
+                  to="/"
+                  className="px-3 py-2 text-dark font-semibold hover:text-[#07d9d6]  text-[15px] transition-all duration-300 ease-in"
+                >
+                  Template1
+                </Link>
+              </div>
+              <div className="end-links">
+                <div className="flex gap-8 items-center">
+                  <Link to="#" className="text-2xl">
+                    <GoSearch />
+                  </Link>
+                  <a href="#" className="bs7-button">
+                    Free Quote
+                  </a>
+                </div>
               </div>
             </div>
           </div>
